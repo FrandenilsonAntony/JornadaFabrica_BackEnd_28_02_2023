ce644665af56c8147fca9f62bcf5f6fc6ec5bc7a
FrandenilsonAntony <iabyantony88@gmail.com>
2023-03-01 15:51:10 -0300

    Listagem de EndPoints e Criação dos EndPoints de Leitura, tanto individual e todos os itens

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ app.get('/', (req, res) => {
   res.send('Hello World Seja bem vindo volte sempre');
 });
 
+/*
+Lista de Endpoints da aplicação CRUD de mensagens
+CRUD: Create, Read(Single & All), Update Delete
+CRUD: Criar, Ler(Individual e Tudo), Atualizar e Remover
+- [GET] /mensagens - Retorna a lista de mensagens
+- [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+- [POST] /mensagens - Cria uma nova mensagem
+- [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+- [DELETE] /mensagens/{id} - Remove uma mensagem pelo ID
+*/
+
+const mensagens = [
+  "Primeira mensagem",
+  "Segunda mensagem"  
+];
+// - [GET] /mensagens - Retorna a lista de mensagens
+app.get('/mensagens', (req, res) =>{
+  res.send(mensagens)
+
+});
+
+//- [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+app.get('/mensagens/:id', (req, res) => {
+  const id = req.params.id - 1;
+  const mensagem = mensagens[id];
+  res.send(mensagem);
+
+});
+
 app.listen(port, () => {
     console.info(`App rodando em http://localhost:${port}`);
 });
